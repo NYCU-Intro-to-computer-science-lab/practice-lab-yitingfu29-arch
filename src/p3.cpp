6c97d5632b6b47f587250ba5eb9b6761726e2aaa
yitingfu29-arch <yitingfu29@gmail.com>
2025-11-24 15:24:41 +0800

    Update p3.cpp

--- src/p3.cpp
+++ src/p3.cpp
@@ -7,15 +7,16 @@ using namespace std;
 // 回傳: 爬到第 n 階的方法數
 // 提示: Base cases: climb(1) = 1, climb(2) = 2
 //       Recursive case: climb(n) = climb(n-1) + climb(n-2)
-int climb(int n) {
-    // 在此實作你的程式碼
-    return 0;
+#include<iostream>
+using namespace std;
+int climb(int n){
+	if (n==1)return 1;
+	if(n==2)return 2;
+	return climb(n-1)+climb(n-2);
 }
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // TODO: 讀取 n 並呼叫 climb 函數，輸出結果
-    
-    return 0;
+int main(){
+	int a;
+	cin>>a;
+	cout<<climb(a);
+	return 0;
 }
