6816231926fb3ab4ae0374f0bdb23199769ea296
yitingfu29-arch <yitingfu29@gmail.com>
2025-11-25 00:59:16 +0800

    Update p2.cpp

--- src/p2.cpp
+++ src/p2.cpp
@@ -7,15 +7,15 @@ using namespace std;
 // 回傳: 1 + 2 + 3 + ... + n 的總和
 // 提示: Base case: n == 1 時回傳 1
 //       Recursive case: 回傳 n + sum(n-1)
-int sum(int n) {
-    // 在此實作你的程式碼
-    return 0;
-}
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // TODO: 讀取 n 並呼叫 sum 函數，輸出結果
-    
-    return 0;
+int sum(int n){
+	if(n==1)return 1;
+	return n+sum(n-1);
+}
+int main(){
+	int x;
+	cin>>x;
+	cout<<sum(x)<<endl;
+	return 0;
+	
 }
